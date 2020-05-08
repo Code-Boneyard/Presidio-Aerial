bced1eb971455b5e7b7e835fc80d7c1b5d939ced
Max Geller <mxgeller@gmail.com>
2020-05-07 17:11:14 -0700

    updated menu links

--- landing/src/containers/home-video/hero/index.js
+++ landing/src/containers/home-video/hero/index.js
@@ -56,7 +56,7 @@ const Hero = (props) => {
                 <ContentLeft>
                   {date && <Text {...descStyle}>{date}</Text>}
                   {title && <Heading {...headingStyle}>{title}</Heading>}
-                  {btn_link && <Button as={Link} to={btn_link} layout={2}>ONLINE SCHEDULING!<i className="ti-arrow-right"></i></Button>}
+                  {btn_link && <Button as={Link} to={btn_link} layout={2}>BOOK SERVICES ONLINE!<i className="ti-arrow-right"></i></Button>}
                 </ContentLeft>
                 <ContentRight>
                   <VideoBtn onClick={modalVideoOpen}>
