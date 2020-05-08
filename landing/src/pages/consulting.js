055a1dff385874cbdcaf464105e475e9fb4124f5
Max Geller <mxgeller@gmail.com>
2020-05-07 17:19:39 -0700

    updated page requirements

--- landing/src/pages/consulting.js
+++ landing/src/pages/consulting.js
@@ -3,7 +3,6 @@ import SEO from '../components/seo';
 import Layout from '../containers/layout/layout'
 import Banner from '../containers/company/banner'
 import About from '../containers/company/about'
-import Team from '../containers/company/team'
 import JoinOurTeam from '../containers/company/join-our-team'
 import Testimonial from '../containers/company/testimonial'
 
@@ -12,7 +11,6 @@ export default () => (
         <SEO title="Company"/>
         <Banner/>
         <About/>
-        <Team/>
         <JoinOurTeam/>
         <Testimonial/>
     </Layout>
