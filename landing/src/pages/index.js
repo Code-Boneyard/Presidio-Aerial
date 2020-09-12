68069c249939ee83c43e3381dd611f256c9bbf0f
Max Geller <mxgeller@gmail.com>
2020-09-12 12:54:34 -0700

    removing teams

--- landing/src/pages/index.js
+++ landing/src/pages/index.js
@@ -6,7 +6,6 @@ import Story from '../containers/home-video/story'
 import ProjectSection from '../containers/home-video/project'
 import TestimonialSection from '../containers/home-video/testimonial'
 import BlogSection from '../containers/home-video/blog'
-import Team from '../containers/company/team'
 import JoinOurTeam from '../containers/company/join-our-team'
 
 export default () => (
@@ -14,7 +13,6 @@ export default () => (
         <SEO title="Presidio Aerial Solutions - San Francisco Drone Photography and Stock Media"/>
         <Hero/>
         <Story/>
-        <Team/>
         <ProjectSection/>
         <TestimonialSection/>
         <JoinOurTeam/>
